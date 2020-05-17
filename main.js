5c4ad7d0ea7989c6ddcad27edb161d50667da396
Oleksii Nesterets <oleksii.nesterets@libertexgroup.com>
2020-05-17 21:57:39 +0300

    some improve

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
 'use strict'
 
-let canBeSame = false;
-
 document.getElementById('submit').addEventListener('click', function (e) {
+    let canBeSame = !document.getElementById('can-be-same').checked;
+    console.log({ canBeSame });
     let mapsNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     const map1 = Math.floor((Math.random() * mapsNumbers.length) + 1);
-    if ([6, 7].includes(map1)) {
-        mapsNumbers = mapsNumbers.filter(number => ![6, 7].includes(number));
-    } else if ([8, 9].includes(map1)) {
-        mapsNumbers = mapsNumbers.filter(number => ![8, 9].includes(number));
-    } else {
-        if (map1 !== 1) {
-            mapsNumbers = mapsNumbers.filter(number => number !== map1);
-        }
-    }
+    mapsNumbers = filterMaps(map1);
+
     const index2 = Math.floor((Math.random() * mapsNumbers.length));
     const map2 = mapsNumbers[index2];
+    mapsNumbers = filterMaps(map2);
+
+    const index3 = Math.floor((Math.random() * mapsNumbers.length));
+    const map3 = mapsNumbers[index3];
+    mapsNumbers = filterMaps(map3);
+
+    const index4 = Math.floor((Math.random() * mapsNumbers.length));
+    const map4 = mapsNumbers[index4];
 
     document.getElementById('player-1').innerText = map1;
     document.getElementById('player-2').innerText = map2;
+    document.getElementById('player-3').innerText = map3;
+    document.getElementById('player-4').innerText = map4;
+
+    
+    function filterMaps(mapNumber) {
+        if ([6, 7].includes(mapNumber)) {
+            return mapsNumbers.filter(number => ![6, 7].includes(number));
+        } else if ([8, 9].includes(mapNumber)) {
+            return mapsNumbers.filter(number => ![8, 9].includes(number));
+        } else {
+            if (mapNumber !== 1 || canBeSame) {
+                console.log('in if');
+                return mapsNumbers.filter(number => number !== mapNumber);
+            }
+            return mapsNumbers;
+        }
+    }
 });
-
-if (!canBeSame) {
-
-}
